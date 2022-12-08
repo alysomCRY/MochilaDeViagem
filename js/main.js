879f018e27e5b7362af2e51c4285b3871ebfed9f
alysom crystyam <alysom.3d@live.com>
2022-12-08 14:39:45 +0000

    atualizacao02

--- js/main.js
+++ js/main.js
@@ -20,10 +20,10 @@ form.addEventListener("submit", (event) => {
     if (existe) {
         itemAtual.id = existe.id
         atualizaElemento(itemAtual);
-
-        itens[existe.id] = itemAtual
+        //Refatoração da condicional if else, atualizando um id para cada item
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
     }else {
-        itemAtual.id = itens.length 
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id +1 : 0 ;
 
          criaElemento(itemAtual);
 
@@ -45,16 +45,32 @@ function criaElemento(item) {
     novoItem.appendChild(numeroDoItem);
     novoItem.innerHTML += item.nome;
 
+    novoItem.appendChild(botaoDeleta(item.id));
+
     lista.appendChild(novoItem); 
 }
 function atualizaElemento(item) {
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
 
 }
+//Função para criar botão com evento de click nos itens, e retornar os itens clicados
+function botaoDeleta (id) {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerHTML = "Apagar"
 
+    elementoBotao.addEventListener("click", function(){
+        deletaElemento(this.parentNode, id)
+    })
+    return elementoBotao;
+}
+//Função para deletar os itens enviados da função botaoDeleta no array de itens e no navegador
+function deletaElemento (tag, id) {
+    tag.remove();
 
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
 
-
+    localStorage.setItem("itens", JSON.stringify(itens));
+}
 
 
 
